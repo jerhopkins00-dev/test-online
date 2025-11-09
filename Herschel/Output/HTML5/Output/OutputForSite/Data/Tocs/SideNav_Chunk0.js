4c627f66c811da661e7a4f1afbc3abd5f8eba4e7
Jeff Steinmann <jerhopkins00@gmail.com>
2025-11-09 10:34:32 -0500

    add built Flare HTML output

---
+++ Herschel/Output/HTML5/Output/OutputForSite/Data/Tocs/SideNav_Chunk0.js
@@ -0,0 +1,1 @@
+define({'/Content/Article_Template.htm':{i:[0],t:['Article Heading'],b:['']},'/Content/Release_Notes.htm':{i:[1],t:['Release Notes'],b:['']},'/Content/System_Requirements.htm':{i:[2],t:['System Requirements'],b:['']}});
